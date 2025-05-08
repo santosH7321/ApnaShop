17a094bb57f1a3e9a98f5a51e7377a34ea0aee2d
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-05-08 13:23:46 +0530

    user and address model schema designe

---
+++ server/models/address.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    address_line:{
+        type: String,
+        default: ""
+    },
+    city:{
+        type: String,
+        default:""
+    },
+    state: {
+        type: String,
+        default: ""
+    },
+    pincode: {
+        type: Number,
+    },
+    country: {
+        type: String,
+    },
+    mobile: {
+        type: Number,
+        default: null
+    }
+}, {timestamps: true})
+
+
+const AddressModel = mongoose.model('address', addressSchema);
+export default AddressModel;
