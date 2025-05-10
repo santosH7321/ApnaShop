a7ee3f9b7ae991c265c8947e08fa7cf6d7d53a71
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-05-10 15:30:26 +0530

    User update routes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -221,3 +221,38 @@ export const uploadAvatar = async(request, response) => {
     })
   }
 }
+
+// upadate user details
+
+export const updateUserDetails = async(request, response) => {
+  try {
+    const userId = request.userId // auth middleware
+    const { name, email, mobile, password } = request.body
+
+    let hashPassword = ""
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      hashPassword = await bcrypt.hash(password, salt);
+    } 
+    
+    const updateUser = await UserModel.findByIdAndUpdate(userId, {
+      ...(name && { name: name}),
+      ...(email && { email: email}),
+      ...(mobile && { mobile: mobile}),
+      ...(password && { password: hashPassword})
+    })
+
+    return response.json({
+      message: "Updated user successfully",
+      error: false,
+      success: true,
+      data: updateUser
+    })
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false
+    })
+  }
+}
